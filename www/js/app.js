89459f808a3fb19e8ee4d3707375b6d773f227f4
Ibrahim <ibpasha1@umbc.edu>
2017-12-19 16:51:24 -0500

    updates to login and sending email to second page

--- www/js/app.js
+++ www/js/app.js
@@ -46,57 +46,25 @@ $("#login").click(function(){
           beforeSend: function(){ $("#login").val('Connecting...');},
           success: function(data)
             {
-                if ($.trim(data) == "success") {
-                  $('#status').html(data);
-                  //localStorage.login="true";
-                  window.location.href = "backbone.html";
-              } else {
-                  $('#status').html(data);
-                  //localStorage.login="false";
-                  window.location.href = "index.html";
-             }
-          }
+              var obj = JSON.parse(data);
+                var current_status = obj.status;
+                var user_email     = obj.email;
+                if (current_status == email) {
+                    $('#status').html(current_status);
+                     console.log(user_email);
+                     window.location.href = "backbone.html";
+                  } else {
+                     window.location.href = "login.html";
+                  }
+            }
+
        });
     }
    return false;
 });
 
 //update_account
-$("#update_account").click(function(){
-    var email=$("#email").val();
-    var password=$("#password").val();
-    var dataString="email="+email+"&password="+password+"&update_account=";
-    if($.trim(email).length>0 & $.trim(password).length>0)
-    {
-        $.ajax({
-          type: "POST",
-          url: url,
-          data: dataString,
-          crossDomain: true,
-          cache: false,
-          beforeSend: function(){ $("#signup").val('Connecting...');},
-          success: function(data){
-            if(data=="success")
-            {
-                alert("Thank you for Registering with us! you can login now");
-                $("#update_account").val('submit');
-                 localStorage.login="true";
-				 localStorage.email=email;
-				 window.location.href = "backbone.html";
-            }
-            else if(data="exist")
-			{
-				alert("Hey! You alreay has account! you can login with us");
-			}
-			else if(data="failed")
-			{
-			alert("Something Went wrong");
-			}
-          }
-      });
-    }
-   return false;
-});
+
 
     $("#logout").click(function(){
      localStorage.login="false";
